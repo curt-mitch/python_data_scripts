ce561f7c8d7a43561690738280651292f1c9e087
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2017-08-08 20:13:51 -0700

    Data extraction by state for Brewers Assocation data

---
+++ Brewers_Association.py
@@ -0,0 +1,21 @@
+# extraction of data from https://www.brewersassociation.org/category/insights/
+
+import xlrd
+import json
+
+data = xlrd.open_workbook('data/Brewers_Association_Brewery_Data.xls')
+sheet = data.sheet_by_name('Capita per Brewery')
+
+data = {}
+for i in xrange(sheet.nrows):
+  row = sheet.row_values(i)
+  if i > 0 and i < 53:
+    state = row[0]
+    data[state] = {
+      'brewery_count': row[4],
+      'breweries_per_capita': row[5]
+    }
+
+with open('data/breweries_per_capita.json', 'w') as fp:
+  json.dump(data, fp)
+
