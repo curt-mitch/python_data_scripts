28000cc342dc335bd0cb9e2fac71ebd555f062b5
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2017-08-09 19:30:49 -0700

    Turn data into array of states data

--- Brewers_Association.py
+++ Brewers_Association.py
@@ -6,15 +6,17 @@ import json
 data = xlrd.open_workbook('data/Brewers_Association_Brewery_Data.xls')
 sheet = data.sheet_by_name('Capita per Brewery')
 
-data = {}
+data = []
 for i in xrange(sheet.nrows):
   row = sheet.row_values(i)
   if i > 0 and i < 53:
-    state = row[0]
-    data[state] = {
+    state_data = {
+      'state': row[0],
       'brewery_count': row[4],
       'breweries_per_capita': row[5]
     }
+    data.append(state_data)
+
 
 with open('data/breweries_per_capita.json', 'w') as fp:
   json.dump(data, fp)
